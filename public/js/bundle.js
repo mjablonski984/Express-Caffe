3ea11025fff884f96a026b32c72178f5258c2a06
mjablonski984 <mrjab84@gmail.com>
2020-04-09 15:03:55 +0100

    Initial commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -31714,8 +31714,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
@@ -31740,58 +31738,37 @@ var _Footer2 = _interopRequireDefault(_Footer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var App = function (_Component) {
-  _inherits(App, _Component);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
-  }
-
-  _createClass(App, [{
-    key: 'render',
-    value: function render() {
-      return _react2.default.createElement(
-        _reactRouterDom.HashRouter,
-        { history: _reactRouterDom.browserHistory },
+function App() {
+  return _react2.default.createElement(
+    _reactRouterDom.HashRouter,
+    { history: _reactRouterDom.browserHistory },
+    _react2.default.createElement(
+      'div',
+      { className: 'App' },
+      _react2.default.createElement(
+        'header',
+        null,
         _react2.default.createElement(
-          'div',
-          { className: 'App' },
+          _reactRouterDom.Link,
+          { to: '/' },
           _react2.default.createElement(
-            'header',
+            'h2',
             null,
-            _react2.default.createElement(
-              _reactRouterDom.Link,
-              { to: '/' },
-              _react2.default.createElement(
-                'h2',
-                null,
-                'Express Caffe'
-              )
-            )
-          ),
-          _react2.default.createElement(
-            'div',
-            { className: 'container' },
-            _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: '/', component: _Landing2.default }),
-            _react2.default.createElement(_reactRouterDom.Route, { path: '/menus/:id', component: _Menu2.default }),
-            _react2.default.createElement(_reactRouterDom.Route, { path: '/employees/:id', component: _Employee2.default })
-          ),
-          _react2.default.createElement(_Footer2.default, null)
+            'Express Caffe'
+          )
         )
-      );
-    }
-  }]);
-
-  return App;
-}(_react.Component);
+      ),
+      _react2.default.createElement(
+        'div',
+        { className: 'container' },
+        _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: '/', component: _Landing2.default }),
+        _react2.default.createElement(_reactRouterDom.Route, { path: '/menus/:id', component: _Menu2.default }),
+        _react2.default.createElement(_reactRouterDom.Route, { path: '/employees/:id', component: _Employee2.default })
+      ),
+      _react2.default.createElement(_Footer2.default, null)
+    )
+  );
+}
 
 exports.default = App;
 
